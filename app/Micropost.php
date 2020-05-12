94b535043a45359c7b4b01d92e1a9997febe4c0a
daikikudoh-cmd <daikikudoh313@gmail.com>
2020-05-12 20:14:02 +0900

    add content

--- app/Micropost.php
+++ app/Micropost.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Micropost extends Model
 {
+
+    protected $fillable = ['content', 'user_id'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
